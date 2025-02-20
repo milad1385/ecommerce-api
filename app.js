6463b4025364a36caac19571da1b2d18cb670a32
Milad salamian <miladsalami1385@gmail.com>
2025-02-20 05:00:57 -0800

    add seller route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { setHeaders } = require("./middlewares/setHeaders");
 const authRouter = require("./routes/auth.routes");
 const userRouter = require("./routes/user.routes");
+const sellerRouter = require("./routes/seller.routes");
 
 app.use(express.urlencoded({ extended: false, limit: "30mb" }));
 app.use(express.json({ limit: "30mb" }));
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //* Routers
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/seller", sellerRouter);
 
 app.use((req, res) => {
   console.log("This path is not found: ", req.path);
