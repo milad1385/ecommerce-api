f620a620cb7ea6ddd97a5a4940565672fc01dac9
Milad salamian <miladsalami1385@gmail.com>
2025-02-20 05:49:33 -0800

    add create new seller controller

--- controllers/seller.controller.js
+++ controllers/seller.controller.js
@@ -1,5 +1,38 @@
+const { errorResponse, successResponse } = require("../helpers/responses");
+const Seller = require("../models/seller");
+const { createSellerValidator } = require("../validators/seller.validator");
+const cities = require("../cities/cities.json");
+
 exports.create = async (req, res, next) => {
   try {
+    const { name, contactDetails, cityId } = req.body;
+    const user = req.user;
+
+    await createSellerValidator.validate(req.body, { abortEarly: false });
+
+    const isExsitingSeller = await Seller.findOne({ user: user._id });
+
+    if (isExsitingSeller) {
+      return errorResponse(res, 422, "This seller is already exist !!!");
+    }
+
+    const city = cities.find((city) => +city.id === +cityId);
+
+    if (!city) {
+      return errorResponse(res, 409, "City is not valid !!");
+    }
+
+    const seller = await Seller.create({
+      cityId,
+      contactDetails,
+      name,
+      user: user._id,
+    });
+
+    return successResponse(res, 201, {
+      message: "Seller created successfully :)",
+      seller,
+    });
   } catch (error) {
     next(error);
   }
