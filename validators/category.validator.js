dcf754e69f5eea2d415620a9742c6c514e6d75c1
Milad salamian <miladsalami1385@gmail.com>
2025-02-21 06:02:09 -0800

    update category validator file

--- validators/category.validator.js
+++ validators/category.validator.js
@@ -1,40 +1,6 @@
 const yup = require("yup");
 
-const categoryFieldsSchema = yup.object().shape({
-  name: yup.string().required("Product field name is required").trim(),
-  slug: yup
-    .string()
-    .required("Category slug is required")
-    .trim()
-    .matches(
-      /^[a-z0-9_-]+$/,
-      "Slug can only contain lowercase letters(a-z), numbers(0-9),underscore (_), and hyphens (-)"
-    )
-    .max(255, "Category slug cannot exceed 255 characters"),
-  description: yup.string().trim(),
-  type: yup
-    .string()
-    .required("Product field type is required")
-    .oneOf(["radio", "selectbox"]),
-  required: yup.boolean().default(false),
-  options: yup.array().when("type", {
-    is: (typeName) => ["radio", "selectbox"].includes(typeName),
-    then: () =>
-      yup
-        .array()
-        .required("selectbox and radio fields need an options array")
-        .min(1, "selectbox and radio fields need at least one option")
-        .of(yup.string()),
-  }),
-  min: yup.number().when("type", {
-    is: "number",
-    then: () => yup.number().required("Number field requires a minimum value"),
-  }),
-  max: yup.number().when("type", {
-    is: "number",
-    then: () => yup.number().required("Number field requires a maximum value"),
-  }),
-});
+
 const categoryFiltersSchema = yup.object().shape({
   name: yup.string().required("Category filter name is required").trim(),
   slug: yup
