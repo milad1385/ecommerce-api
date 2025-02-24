b87bed31b81ce23f34108b95ec6ec8f1c4f6357e
Milad salamian <miladsalami1385@gmail.com>
2025-02-24 07:46:08 -0800

    add image uploader in product routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
+const { multerStorage } = require("../middlewares/multer");
 const roleGaurd = require("../middlewares/roleGaurd");
 const controller = require("../controllers/product.controller");
 const router = express.Router();
 
-router.route("/").post(auth, roleGaurd("ADMIN"), controller.create);
+const uploader = multerStorage("public/images/product");
+
+router
+  .route("/")
+  .post(
+    auth,
+    roleGaurd("ADMIN"),
+    uploader.array("images", 10),
+    controller.create
+  );
 
 module.exports = router;
