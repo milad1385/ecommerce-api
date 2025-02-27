68984ab007cb0603bad872d8123dab8da87fc49b
Milad salamian <miladsalami1385@gmail.com>
2025-02-27 11:02:40 -0800

    add comment Schema

---
+++ models/comment.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const replyCommentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5,
+    },
+    reply: {
+      type: [replyCommentSchema],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
