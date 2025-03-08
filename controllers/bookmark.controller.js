5a7499597ceaec16ab3327aa227e9b74c764ae2d
Milad salamian <miladsalami1385@gmail.com>
2025-03-08 09:44:58 -0800

    add addToBookmark controller

--- controllers/bookmark.controller.js
+++ controllers/bookmark.controller.js
@@ -1,3 +1,8 @@
+const { isValidObjectId } = require("mongoose");
+const { errorResponse, successResponse } = require("../helpers/responses");
+const Bookmark = require("../models/bookmark");
+const Product = require("../models/product");
+
 exports.getAllBookmarks = async (req, res, next) => {
   try {
   } catch (error) {
@@ -7,6 +12,37 @@ exports.getAllBookmarks = async (req, res, next) => {
 
 exports.addToBookmark = async (req, res, next) => {
   try {
+    const { productId } = req.params;
+    const user = req.user;
+
+    if (!isValidObjectId(productId)) {
+      return errorResponse(res, 400, "Product Id is not valid :(");
+    }
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) {
+      return errorResponse(res, 404, "This product is not found !!!");
+    }
+
+    const isExistBookmark = await Bookmark.findOne({
+      user: user._id,
+      product: productId,
+    });
+
+    if (isExistBookmark) {
+      return errorResponse(res, 400, "This product is already bookmarked !!!");
+    }
+
+    const bookmark = await Bookmark.create({
+      user: user._id,
+      product: productId,
+    });
+
+    return successResponse(res, 200, {
+      message: "Bookmark added successfully :)",
+      bookmark,
+    });
   } catch (error) {
     next(error);
   }
