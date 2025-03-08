2d2b3db3a9173ae689f1da0274f66ad5a76afcac
Milad salamian <miladsalami1385@gmail.com>
2025-03-08 09:27:18 -0800

    add Bookmark model

---
+++ models/bookmark.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Bookmark", schema);
