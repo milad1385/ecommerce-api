a40a5b1e78610b8035710fe66cb52cc5ccbca102
Milad salamian <miladsalami1385@gmail.com>
2025-03-09 12:41:34 -0700

    add create social controller

--- controllers/social.controller.js
+++ controllers/social.controller.js
@@ -1,5 +1,15 @@
-const { successResponse } = require("../helpers/responses");
-const Social = require("../models/socialMedia");
+const { successResponse, errorResponse } = require("../helpers/responses");
+const SocialMedia = require("../models/socialMedia");
+const { createSocialValidator } = require("../validators/social.validator");
+
+const supportedFormats = [
+  "image/jpeg",
+  "image/png",
+  "image/gif",
+  "image/webp",
+  "image/svg+xml",
+  "application/octet-stream",
+];
 
 exports.getAll = async (req, res, next) => {
   try {
@@ -13,7 +23,32 @@ exports.getAll = async (req, res, next) => {
 
 exports.createSocial = async (req, res, next) => {
   try {
-    
+    const { name, link } = req.body;
+    await createSocialValidator.validate({ name, link }, { abortEarly: false });
+
+    if (!req.file) {
+      return errorResponse(
+        res,
+        400,
+        `Category Icon is required - field:"icon"`
+      );
+    }
+    const { filename, mimetype } = req.file;
+    if (!supportedFormats.includes(mimetype)) {
+      return errorResponse(res, 400, "Unsupported image format");
+    }
+
+    const icon = {
+      filename: filename,
+      path: `images/socials/${filename}`,
+    };
+
+    const newSocial = await SocialMedia.create({ icon, name, link });
+
+    return successResponse(res, 201, {
+      social: newSocial,
+      message: "social media added successfully",
+    });
   } catch (error) {
     next(error);
   }
