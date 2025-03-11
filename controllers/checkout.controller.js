9e912d3276cc466c8ff1982fa85caa39e92d3a97
Milad salamian <miladsalami1385@gmail.com>
2025-03-11 09:40:15 -0700

    add createCheckout controller

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -1,15 +1,64 @@
+const { errorResponse } = require("../helpers/responses");
+const Cart = require("../models/cart");
+const Checkout = require("../models/checkout");
 const { createPayment } = require("../services/zarinpal");
 
 exports.createCheckout = async (req, res, next) => {
   try {
-    const result = await createPayment({
-      amountInRial: 1000,
-      description: "توضیحات تستی برای تست پرداخت",
+    const user = req.user;
+    const { shippingAddress } = req.body;
+
+    const cart = await Cart.findOne({ user: user._id }).populate(
+      "items.product items.seller"
+    );
+
+    if (!cart?.items?.length) {
+      return errorResponse(res, 404, "User basket is not found or empty!!!");
+    }
+
+    const checkoutItems = [];
+
+    for (const item of cart.items) {
+      const { product, seller } = item;
+
+      const sellerDetails = product.sellers.find(
+        (sellerInfo) => sellerInfo.seller.toString() === seller._id.toString()
+      );
+
+      if (!sellerDetails) {
+        return errorResponse(res, 400, "Seller is not found !!!");
+      }
+
+      checkoutItems.push({
+        product: product._id,
+        seller: seller._id,
+        quantity: item.quantity,
+        price: sellerDetails.price,
+        discount: item.discount,
+      });
+    }
+
+    const newCheckout = new Checkout({
+      items: checkoutItems,
+      shipping: shippingAddress,
+      user: user._id,
+    });
+
+    const payment = await createPayment({
+      amountInRial: newCheckout.totalPrice,
+      description: `سفارش با شناسه ${newCheckout._id}`,
       mobile: "09336085012",
     });
 
-    console.log(result);
-    
+    newCheckout.authority = payment.authority;
+
+    await newCheckout.save();
+
+    return successResponse(res, 201, {
+      message: "Checkout created successfully :))",
+      checkout: newCheckout,
+      paymentUrl: payment.paymentUrl,
+    });
   } catch (error) {
     next(error);
   }
