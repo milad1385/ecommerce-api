08abc0a86cb54573fa99f4b41549b9d812bb389c
Milad salamian <miladsalami1385@gmail.com>
2025-03-11 11:36:41 -0700

    add getAllUserOrder controller

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,30 @@
+const { successResponse } = require("../helpers/responses");
+const Order = require("../models/order");
+const { createPagination } = require("../utils/pagination");
+
 exports.getAllUserOrder = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    const { status = "all", page = 1, limit = 10 } = req.query;
+
+    const filters = { user: user._id };
+
+    if (status !== "all") {
+      filters.status = status;
+    }
+
+    const orders = await Order.find(filters)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const totalUserOrdersCount = await Order.countDocuments(filters);
+
+    return successResponse(res, 200, {
+      orders,
+      pagination: createPagination(page, limit, totalUserOrdersCount, "Orders"),
+    });
   } catch (error) {
     next(error);
   }
