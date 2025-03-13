eb6dd4970b84d393bd73fc8defd05e5668563add
Milad salamian <miladsalami1385@gmail.com>
2025-03-13 14:29:29 -0700

    add getAllUserTickets controller

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,9 +1,35 @@
 const Ticket = require("../models/ticket");
 const Department = require("../models/department");
 const SubDepartment = require("../models/subdepartment");
+const { successResponse } = require("../helpers/responses");
+const { createPagination } = require("../utils/pagination");
 
 exports.getAllUserTickets = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    const { page = 1, limit = 10, status = "all" } = req.query;
+
+    const filters = {
+      user: user._id,
+    };
+
+    if (status !== "all") {
+      filters.status = status;
+    }
+
+    const tickets = await Ticket.find(filters)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .populate("user department subDepartment replies.user", "-addresses")
+      .sort({ createdAt: -1 });
+
+    const totalTicketsCount = await Ticket.countDocuments(filters);
+
+    return successResponse(res, 200, {
+      tickets,
+      pagination: createPagination(page, limit, totalTicketsCount, "Tickets"),
+    });
   } catch (error) {
     next(error);
   }
