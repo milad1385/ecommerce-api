e5c295db9db50810ecf52daad37c9aa60ea451af
Milad salamian <miladsalami1385@gmail.com>
2025-03-14 14:36:09 -0700

    add article model

---
+++ models/Article.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema(
+  {
+    categories: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ArticleCategory",
+        required: true,
+      },
+    ],
+    title: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    shortName: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: ["published", "draft", "reject"],
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Article", articleSchema);
