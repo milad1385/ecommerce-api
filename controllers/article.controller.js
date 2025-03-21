9afd412b4993444c3de62211ea1e9445bb4303da
Milad salamian <miladsalami1385@gmail.com>
2025-03-21 14:24:53 -0700

    add deleteArticle controller

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,7 +1,7 @@
 const { isValidObjectId } = require("mongoose");
 const Article = require("../models/Article");
 const ArticleCategory = require("../models/ArticleCategory");
-const { errorResponse } = require("../helpers/responses");
+const { errorResponse, successResponse } = require("../helpers/responses");
 
 exports.getAllPublishedArticles = async (req, res, next) => {
   try {
@@ -26,6 +26,26 @@ exports.getAllArticles = async (req, res, next) => {
 
 exports.deleteArticle = async (req, res, next) => {
   try {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return errorResponse(res, 400, "Please send valid id");
+    }
+
+    const deletedArticle = await Article.findByIdAndDelete(id);
+
+    if (!deletedArticle) {
+      return errorResponse(res, 404, "Article is not found !!!");
+    }
+
+    fs.unlink(`public/images/articles/${deletedArticle?.cover}`, (err) =>
+      next(err)
+    );
+
+    return successResponse(res, 200, {
+      message: "Article deleted successfully :)",
+      article: deletedArticle,
+    });
   } catch (error) {
     next(error);
   }
